7ef5e7dbd5b9dacb16e3dddcffc1a53e41f5f7b2
Kelly Farman <kelfarman@gmail.com>
2021-03-15 13:48:56 -0400

    added Navbar and Fontawesome

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,12 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
 });
-
-$("#reserveButton").click(function(){
-     $("#reserveModal").modal("show");
-})
-
-$("#loginButton").click(function(){
-    $("#loginModal").modal("show");
-})
